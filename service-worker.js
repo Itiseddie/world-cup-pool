92c2d7355a24a07325118c4b2405bc66ee58cc3a
Itiseddie <101852930+Itiseddie@users.noreply.github.com>
2026-06-11 15:40:12 -0700

    Refresh schedule preview cache

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "world-cup-pool-v11-schedule-scores";
+const CACHE_NAME = "world-cup-pool-v12-schedule-scores";
 const ASSETS = [
   "./",
   "./index.html",
@@ -6,7 +6,7 @@ const ASSETS = [
   "./styles.css?v=schedule-filter",
   "./schedule-polish.css?v=6",
   "./app.js?v=schedule-filter",
-  "./schedule-polish.js?v=9",
+  "./schedule-polish.js?v=10",
   "./manifest.json",
   "./icons/icon-192.png",
   "./icons/icon-512.png"
